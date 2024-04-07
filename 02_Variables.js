4ddba39773f2a3f1e1ce13680817721fba176abc
bhanubhashkar <reachoutbhanu@gmail.com>
2024-04-08 00:50:04 +0530

    datatypes

--- 02_Variables.js
+++ 02_Variables.js
@@ -1,27 +1,44 @@
 
+//#####----- JavaScript Variables -----------------------------------------------------------------------------------------------------------#####
+
+//Const   -   Once initialised cant be changed during program execution/run time.
+//Let     -   Globally used, Value can be changed during runtime. Solved block and functional scope issue. 
+//Var     -   Not preferred, Scope issue, changed all values of that variable in all blokcks and functions.
+
 //Variable Declaration
+console.log("### Declearing and initializing the variables...");
 const userName = "Bhanu Bhashkar";
 let userPass = "Password@01";
 var userEmail = "bhanu@email.com";
 userCity = "City";
+console.log("Completed declearation and initialization of the variables.");
 
-//Looging value in console
-console.log(userName);
-console.log(userPass);
-console.log(userEmail);
-console.log(userCity);
-
-//Changing the values
-// userName = "Shashank Shekhar"; //TypeError: Assignment to constant variable.
-userPass = "NewPassword@09";
-userEmail = "Bhashkar@email.com";
-userCity = "New City";
-
-//Logging after value change in console
+//Looging the original value in console
+console.log("### Looging the original value in console :");
 console.log(userName);
 console.log(userPass);
 console.log(userEmail);
 console.log(userCity);
 
 //Log multiple variables in one go
+console.log("### Looging the original value in console as table :");
+console.table([userName, userEmail, userPass, userCity]);
+
+//Changing the values
+console.log("### Changing the values...");
+// userName = "Shashank Shekhar"; //TypeError: Assignment to constant variable.
+userPass = "NewPassword@09";
+userEmail = "Bhashkar@email.com";
+userCity = "New City";
+console.log("Value changed.");
+
+//Logging after value change in console
+console.log("### Looging the changed value in console :");
+console.log(userName);
+console.log(userPass);
+console.log(userEmail);
+console.log(userCity);
+
+//Log multiple variables in one go
+console.log("### Looging the changed value in console as table :");
 console.table([userName, userEmail, userPass, userCity]);
---
+++ 03_DataTypes.js
@@ -0,0 +1,40 @@
+
+//#####----- JavaScript DataTypes -----------------------------------------------------------------------------------------------------------#####
+
+//Type	    typeof return value	    Object wrapper
+//Number    "number"	            Number
+//BigInt    "bigint"	            BigInt
+//String    "string"	            String
+//Boolean   "boolean"	            Boolean
+//Null	    "object"	            N/A
+//Undefined "undefined"	            N/A
+//Symbol    "symbol"	            Symbol
+//Object    "object"                Object
+
+//Declaration initialization of difeferent data types
+console.log("### Declearing and initializing the variables...");
+let this_num = 42;              // This is a number
+let this_int = BigInt(55);      // This is a bigint
+let this_str = "I am a String"; // This is a string
+let this_bool = true;           // This is a boolean
+let this_null = null;           // This is null
+let this_undef = undefined;     // This is undefined
+let this_sym = Symbol("sym");        // This is symbol
+let this_obj = {Values: "Bhanu", address: "Kolkata"};   // This is object
+console.log("Completed declearation and initialization of the variables.");
+
+//Logging value of difeferent data types in table
+console.log("### Logging the variables in table.");
+console.table([this_num, this_int, this_str, this_bool, this_null, this_undef, this_sym, this_obj])
+
+//Logging typeof values of difeferent variables
+console.log("### Logging the typeof value of variables...");
+console.log(typeof(this_num));
+console.log(typeof(this_int));
+console.log(typeof(this_str));
+console.log(typeof(this_bool));
+console.log(typeof(this_null));
+console.log(typeof(this_undef));
+console.log(typeof(this_sym));
+console.log(typeof(this_obj));
+console.log("Complete logging the typeof value of variables.");
