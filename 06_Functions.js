9bd43b600c637eaf41583308849b7f91a8c397b4
Bhanu Bhashkar <reachoutbhanu@gmail.com>
2024-12-24 17:37:20 +0530

    changes

--- 06_Functions.js
+++ 06_Functions.js
@@ -2,21 +2,54 @@
 // #####----- Functions --------------------------------------------------------------------------------------#####
 
 /*
-A function is a reusable block of code that performs a specific task. Its defined once and can run (or “call”) whenever same task needs to be done in program.
-
+A function is a reusable block of code designed to perform a particular task. Functions can take inputs, process them, and return a result.
+Its defined once and can run (or “call”) whenever same task needs to be done in program.
 
 */
 
 // Simple Function
-function greetUser(user){
-    console.log(`Hey ${user}, welcome.`);
-    return(null);
+function welcomeUser(){
+    console.log("welcomeUser function executed.");
+    return(`Hey welcome.`);
 }
 
-greetUser('Bhanu');
+let welcome = welcomeUser();
+console.log(welcome);
 
 
+// Function with argument
+function greetUser(user,msg){
+    console.log("greetUser function executed.");
+    return(`Hey ${user}, Good ${msg}...`);
+}
 
+let greet = greetUser("Bhanu","Morning");
+console.log(greet);
 
+// Function with function in return
+function userLog(log){
+    function user(){
+        return(`User : ${log}`)
+    }
+    return user();
+}
+
+let log = userLog("logged in");
+console.log(log);
+
+// Arrow Function 
+const newGreetUser = (user,msg) => {
+    console.log("newGreetUser function executed.");
+    return(`Hey ${user}, Good ${msg}...`);
+}
+
+let newGreet = newGreetUser("Bhashkar","Evening");
+console.log(newGreet);
+
+// Single line Arrow Function 
+const newWelcome = (user) => `Welcome ${user}.`
+
+let msg = newWelcome("Bhanu")
+console.log(msg);
 
 // #####------------------------------------------------------------------------------------------------------#####
---
+++ 07_HigherOrderFunctions.js
@@ -0,0 +1,13 @@
+// #####------------------------------------------------------------------------------------------------------#####
+// #####----- Higher Order Functions -------------------------------------------------------------------------#####
+
+/*
+Higher-Order Functions are functions that can accept other functions as arguments, return functions, or both. 
+They enable abstraction and flexibility in code, allowing to create reusable and modular functions for complex operations, making them essential in functional programming.
+*/
+
+
+
+
+
+// #####------------------------------------------------------------------------------------------------------#####
