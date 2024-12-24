8d2b22850153b1d9f891cf524b28ceb77d4aa83a
Bhanu Bhashkar <reachoutbhanu@gmail.com>
2024-12-24 13:00:05 +0530

    file added

---
+++ 06_Functions.js
@@ -0,0 +1,22 @@
+// #####------------------------------------------------------------------------------------------------------#####
+// #####----- Functions --------------------------------------------------------------------------------------#####
+
+/*
+A function is a reusable block of code that performs a specific task. Its defined once and can run (or “call”) whenever same task needs to be done in program.
+
+
+*/
+
+// Simple Function
+function greetUser(user){
+    console.log(`Hey ${user}, welcome.`);
+    return(null);
+}
+
+greetUser('Bhanu');
+
+
+
+
+
+// #####------------------------------------------------------------------------------------------------------#####
